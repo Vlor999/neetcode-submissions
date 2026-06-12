20c13a9425b42bf8dca6630f12a542ef2bcce04f
willem adnet <140308890+Vlor999@users.noreply.github.com>
2026-06-12 09:56:52 +0200

    Add: same-binary-tree - submission-2

---
+++ Data Structures & Algorithms/same-binary-tree/submission-2.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def linearize(self, p:TreeNode) -> list:
+        if p is None:
+            return []
+        lin = []
+        nexts = [p]
+        while nexts:
+            node = nexts.pop(0)
+            if node:
+                nexts.append(node.left)
+                nexts.append(node.right)
+            lin.append(node.val if node else None)
+        print(lin)
+        return lin
+
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        linearPTree = self.linearize(p)
+        linearQTree = self.linearize(q)
+        return linearQTree == linearPTree
+        
